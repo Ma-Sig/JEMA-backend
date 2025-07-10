49340ca4168f4c4f33b5b5b2e45e9fd10d8fe390
Jonnathan Cuzco <jonnathan.cuzcot@outlook.com>
2025-07-10 15:15:48 -0500

    Probando github actions

---
+++ run-tests.bat
@@ -0,0 +1,3 @@
+@echo off
+cd bruno-testing
+npx @usebruno/cli run .\Unidades --env-file .\environments\JEMA.bru
---
+++ run-tests.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd bruno-testing
+npx @usebruno/cli run .\Unidades --env-file .\environments\JEMA.bru
